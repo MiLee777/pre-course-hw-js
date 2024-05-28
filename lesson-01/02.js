3c75215de1b7693bd1e33b67417e6149383fd6cf
MiLee777 <midyashka1506@gmail.com>
2024-05-28 22:28:17 +0700

    fix mistakes in 2 and 3 tasks

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,5 +1,7 @@
 let number1 = Math.floor(Math.random() * 100);
-console.log(number1%2 === 0 ? `${number1}:Это четное число` : `${number1}:Это нечетное число`);
-
-
+if (number1 % 2 === 0) {
+  console.log(`${number1} : Это четное число`);
+} else {
+  console.log(`${number1} : Это нечетное число`);
+}
 
