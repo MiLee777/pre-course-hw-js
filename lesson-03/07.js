fef2dd3c7a7a3eae32b1ea9332784e3b2bf88fd9
MiLee777 <midyashka1506@gmail.com>
2024-07-03 19:29:36 +0700

    add solution to the task 7 of lesson 3

---
+++ lesson-03/07.js
@@ -0,0 +1,12 @@
+const numbers = [
+  [1, 2, 3, 4, 5],
+  [11, 20, 33, 40, 55],
+  [111, 200, 333, 400, 555],
+];
+
+let newArr = [];
+for (const num of numbers) {
+  newArr.push(num.filter(i => i%2 ===0))
+}
+
+console.log(newArr);
